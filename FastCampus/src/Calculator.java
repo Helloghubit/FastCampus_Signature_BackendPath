573a4ea57f045661a68cb58c2f71a6d973e74501
unknown <rnjsxorals1@gmail.com>
2024-06-05 18:38:40 +0900

    course 1 ch2

---
+++ FastCampus/src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a, b, tot;
+        a = 3;
+        b = 2;
+        tot = a+b;
+        System.out.println("a + b = " + tot);
+    }
+}
