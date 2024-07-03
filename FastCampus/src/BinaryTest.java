6c42b3345ab43b5da71141976a070e122a51a464
unknown <rnjsxorals1@gmail.com>
2024-07-03 18:55:03 +0900

    ss

---
+++ FastCampus/src/BinaryTest.java
@@ -0,0 +1,20 @@
+public class BinaryTest {
+    public static void main(String[] args) {
+        int i = 69;
+        System.out.println();
+    }
+
+    private static void jinsu(int i, int j){
+        StringBuilder sb = new StringBuilder();
+        int a = 1;
+        int count = 0;
+        while(a*j<i) {
+            a*=j;
+            count ++;
+        }
+        int[] b = new int[count];
+        while(count>0){
+            
+        }
+    }
+}
---
+++ FastCampus/src/FindAPI.java
@@ -0,0 +1,8 @@
+public class FindAPI {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        java.lang.String s; //java.lang --> 패키지
+        s = "apple";
+        System.out.println(s);
+    }
+}
